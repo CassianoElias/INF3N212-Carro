5553f09fa3257ebd8a080cddeafa16a82ee60a10
Cassiano Elias Santana <201401665@senacinformatica.br>
2023-03-30 21:40:46 -0300

    Adição de Switch case e while nos menus. ( Fluxo dos metodos main)

--- src/inf3n212/carro/INF3N212Carro.java
+++ src/inf3n212/carro/INF3N212Carro.java
@@ -26,12 +26,68 @@ public class INF3N212Carro {
         // TODO code application logic here
         cadPessoa.mockPessoas();
         cadCarro.mockCarros();
-        
-        System.out.println(cadPessoa.getPessoa());
-        System.out.println("----");
-        System.out.println(cadCarro.getCarros());
-        
-                
+        int opM = 99;
+        int opSM = 99;
+    do{
+        System.out.println("\n-- Sistema de Cadastro --");
+        menuP();
+        opM = leiaNumInt();
+        switch (opM) {
+            case 1: 
+            case 2:
+                do{
+                subMenu(opM);
+                opSM = leiaNumInt();
+                switch (opSM) {
+                case 1:
+                    System.out.println(" -- Cadastrar -- ");
+                    if (opM == 1) {
+                        cadastraPessoa();
+                    }else{
+                        cadastrarCarro();
+                    }
+                    break;
+                case 2:
+                    System.out.println(" -- Editar -- ");
+                    if (opM == 1) {
+                        editarPessoa();
+                    }else{
+                        editarCarro();
+                    }
+                    break;
+                case 3:
+                    System.out.println(" -- Listar -- ");
+                    if (opM == 1) {
+                        listarPessoa();
+                    }else{
+                        listarCarro();
+                    }
+                    break;
+                case 4:
+                    System.out.println(" -- Deletar -- ");
+                    if (opM == 1) {
+                        deletarPessoa();
+                    }else{
+                        deletarCarro();
+                    }
+                    break;
+                case 0:
+            
+                    break;    
+                default:
+                    System.out.println("Opção invalida, tente novamente");
+            }
+           }while(opSM != 0);
+                break;
+            case 0:
+                System.out.println("Aplicação encerrada pelo usuário");
+                break;
+            default:
+                System.out.println("Opção invalida, tente novamente");
+        }
+    }while(opM != 0);
+       
+
     }// fim metodo main
     
     public static int leiaNumInt(){
@@ -63,12 +119,44 @@ public class INF3N212Carro {
             }
             
             System.out.println("-- Ger. " + subM + "--");
-            System.out.println("1 - Cadastrar" + subM);
-            System.out.println("2 - Editar" +subM);
-            System.out.println("3 - Listar" +subM + "s");
-            System.out.println("4 - Deletar" + subM);
-            System.out.println("0 - Voltar");
+            System.out.println("1 - Cadastrar " + subM);
+            System.out.println("2 - Editar " +subM);
+            System.out.println("3 - Listar " +subM + "s");
+            System.out.println("4 - Deletar " + subM);
+            System.out.println("0 - Voltar ");
             System.out.print("Digite Aqui: ");
     }//fim subMenu
+
+    private static void cadastraPessoa() {
+        System.out.println("Cadastre a Pessoa");
+    }
+
+    private static void cadastrarCarro() {
+        System.out.println("Cadastre o Carro");
+    }
+
+    private static void editarPessoa() {
+        System.out.println("Editar");
+    }
+
+    private static void editarCarro() {
+        System.out.println("Editar");
+    }
+        
+    private static void listarPessoa() {
+        System.out.println("Listar");
+    }
+
+    private static void listarCarro() {
+        System.out.println("Listar");
+    }
+
+    private static void deletarPessoa() {
+        System.out.println("Listar");
+    }
+
+    private static void deletarCarro() {
+        System.out.println("Listar");
+    }
     
 }// fim classe
